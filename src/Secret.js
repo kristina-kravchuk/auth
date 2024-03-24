30518b30227a34deac815bd5dc2e34e9478039af
kristina-kravchuk <kristinakravchuk02@gmail.com>
2024-03-23 22:51:36 -0400

    debugging Login.js and Secret.js files

--- src/Secret.js
+++ src/Secret.js
@@ -1,10 +1,11 @@
 import { useAuth0 } from "@auth0/auth0-react";
-const Secret = () => {
+  const Secret = () => {
 
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
+    const { isAuthenticated } = useAuth0();
     return(
-        !isAuthenticated && (
+        !isAuthenticated && 
         <p>Classified!</p>
-    ))
+    )
 }
+
 export default Secret;
